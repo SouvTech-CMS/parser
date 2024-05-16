aa9f21f39042885c1310c6584ed9803d233d3ede
Dusky FOX <mr.dusky.fox@gmail.com>
2024-05-16 19:14:42 +0300

    fix: SKU before SKU data
    Signed-off-by: Dusky FOX <mr.dusky.fox@gmail.com>

--- src/utils/format_order_data.py
+++ src/utils/format_order_data.py
@@ -32,7 +32,7 @@ def format_order_data(order: dict, shop_id: int, ):
             # Creating description for new good
             description = f"Title: {trans['title']} Description: {trans['description']}"
             # Name of new good
-            name = f"{trans['sku']} + "
+            name = f"SKU: {trans['sku']} + "
             # Creating Name for new Good
             for variation in trans["variations"]:
                 if variation['formatted_name'] != "Personalization":
