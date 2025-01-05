35a40f3f1ce45d1b2b4f6d9515ed913175d20c22
fox <78961902+foxdusky@users.noreply.github.com>
2025-01-05 23:39:50 +0300

    refactor: order & good in order schemes

--- src/schemes/order.py
+++ src/schemes/order.py
@@ -2,20 +2,11 @@ from pydantic import BaseModel
 
 
 class Order(BaseModel):
-    id: int | None = None
-    status: str | None = None
-    shop_id: int | None = None
     order_id: str | None = None
+    status: str | None = None
     date: str | None = None
     quantity: int | None = None
     buyer_paid: float | None = None
     tax: float | None = None
-    shipping: float | None = None
-    full_fee: float | None = None
-    profit: float | None = None
-
-
-class OrderUpdate(BaseModel):
-    id: int | None
-    status: str | None
-    shipping: float | None
+    shipping_id: str | None = None
+    tracking_code: str | None = None
