577ceded74833f6834b1e57d6b9e3e39be782d91
Trydimas <akentev.dima@bk.ru>
2025-04-24 18:42:13 +0300

    control refactor

--- src/parser_debug.py
+++ src/parser_debug.py
@@ -1,51 +1,33 @@
-import concurrent.futures
 import json
-import pprint
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from api.parser import update_parser_status_by_id
 from constants.status import ParserStatus
-from etsy_api.orders import get_all_orders_by_shop_id
-from schemes.upload_order import UploadingOrderData, OrderData
-from utils.format_order_data import format_order_data
+from schemes.shop_data import ShopData
+from schemes.upload_order import UploadingOrderData
 from utils.parser_shops_data import get_parser_shops_data
-from utils.excel.write_each_row import write_to_excel
-from configs import settings
 from amazon_api.get_amazon_api import OrderClient
 
-
-
-from sp_api.util import throttle_retry, load_all_pages
-
-from configs import settings
-from constants.amazon_credentials import CREDENTIALS_ARG
 from constants.amazon_dates import LAST_MONTH_DATE, EARLIEST_DATE
-from utils.safe_ratelimit_amazon import safe_rate_limit
-from utils.format_datetime import is_iso_utc_z_format
 from log.logger import logger
-
 
 
 # Every 30 minutes
 PARSER_WAIT_TIME_IN_SECONDS = 60 * 30
-CREATED_AFTER = datetime(2024, 1, 1).isoformat().replace("+00:00", 'Z')
 
 EXCEL_FILE = "data/check_point.xlsx" #temporaty
 
 
 #TODO link on update refresh token don't forget!!!!
 
-def process_single_shop(shop):
+def process_single_shop(shop: ShopData):
 
-	# Initializing constants
+	order_cl = OrderClient(shop=shop)
+	created_after = LAST_MONTH_DATE
 	shop_error = False
-	start_time_shop = datetime.now()
-	order_cl = OrderClient(log=logger)
-
 	offset = 0
-	created_after = EARLIEST_DATE
-	# weekday = datetime.now().weekday()
-	##########################
+	start_time_shop = datetime.now()
+
 
 	logger.info(f"Parsing shop {shop.shop_id} - {shop.shop_name}...")
 	logger.info(
@@ -59,11 +41,8 @@ def process_single_shop(shop):
 
 	logger.success(f"Parser status updated.")
 
-
 	uploading_orders = UploadingOrderData(shop_id=shop.shop_id, orders_data=[])
 
-
-	#loop
 	for page_orders in order_cl.load_all_orders(CreatedAfter=created_after):
 		"""Every 100 orders after <CreatedAfter>"""
 
@@ -77,27 +56,14 @@ def process_single_shop(shop):
 			shop_error = True
 			break
 
-		#TODO подготовить upload
-		uploading_orders.orders_data.append(
-			OrderData(
-				order=order,
-				client=client,
-				city=city,
-				order_items=goods_in_order,
-			)
-		)
+		uploading_orders.orders_data.extend(orders_data)
 
-		# res = upload_orders_data(uploading_orders)
 		with open("test_data.json", "w") as f:
 			json.dump(uploading_orders.model_dump(), f)
 
 
 		offset += 100
 
-		if offset > 200:
-			if now_hour == 20 and weekday in (5, 6):
-				continue
-			break
 
 	if shop_error:
 		logger.error(f"Shop {shop.shop_id} - {shop.shop_name} parsed with error.")
@@ -118,22 +84,6 @@ def process_single_shop(shop):
 	logger.info(
 		f"Shop  {shop.shop_id} - {shop.shop_name} parsing time: {end_time_shop - start_time_shop}"
 	)
-
-
-def etsy_api_parser():
-	shops_data = get_parser_shops_data()
-
-	# Используем ThreadPoolExecutor для параллельной обработки
-	with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-		# Запускаем обработку каждого магазина в отдельном потоке
-		futures = [executor.submit(process_single_shop, shop) for shop in shops_data]
-		# Ждем завершения всех задач
-		concurrent.futures.wait(futures)
-		# Проверяем, были ли исключения
-		for future in futures:
-			if future.exception():
-				logger.error(f"Error in thread: {future.exception()}")
-	logger.success(f"Parsed all shops waiting {PARSER_WAIT_TIME_IN_SECONDS} to repeat")
 
 
 if __name__ == "__main__":
