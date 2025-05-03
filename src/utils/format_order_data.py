9a022143eecb614475d5570a82e1defb7b662bcf
Trydimas <akentev.dima@bk.ru>
2025-05-03 18:07:08 +0300

    fix: format_order_data types

--- src/utils/format_order_data.py
+++ src/utils/format_order_data.py
@@ -1,54 +1,61 @@
-import json
-from datetime import datetime
-
 from schemes.order import Order
 from schemes.client import Client
 from schemes.city import City
 from schemes.order_item import GoodInOrder
 from schemes.upload_order import OrderData
 from utils.format_datetime import iso_to_simple
+from constants.status import OrderStatus
 
 
 def _format_order(*,
                   order: dict, order_obj: Order):
     """earliest fill order_obj"""
-    order_obj.buyer_paid = None
-    order_obj.order_id = order["AmazonOrderId"]
-    order_obj.status = order["OrderStatus"]  # TODO может отличаться от бэка
-    order_obj.date = iso_to_simple(order["PurchaseDate"])
+    order_obj.buyer_paid = float(order.get("OrderTotal", {}).get("Amount", 0))
+    order_obj.order_id = order.get("AmazonOrderId", "")
+    order_obj.status = OrderStatus.get_backend_status(order.get("OrderStatus"))
+    order_obj.date = iso_to_simple(order.get("PurchaseDate"))
     order_obj.quantity = 0
     order_obj.tax = 0
+
+
+    #TODO на бэке сделать ручку под amazon
 
 
 def _format_good_in_order(*,
                           item: dict,
                           item_obj: GoodInOrder):
     """fill good_in_order obj"""
-    item_obj.uniquename = item["SellerSKU"]
-    item_obj.quantity = item["QuantityOrdered"]
-    if item.get("ItemPrice") and item.get("PromotionDiscount"):
-        item_obj.amount = (
-            (float(item["ItemPrice"]["Amount"]) * item["QuantityOrdered"]) - float(item["PromotionDiscount"]["Amount"])
+
+    item_price = item.get("ItemPrice")
+    item_quantity= item.get("QuantityOrdered")
+    item_discount = item.get("PromotionDiscount")
+
+    item_obj.uniquename = item.get("SellerSKU")
+    item_obj.quantity = item_quantity
+
+    _amount = 0.0
+    if item_price and item_discount and item_quantity:
+        _amount = (
+            (float(item_price["Amount"]) * item_quantity) - float(item_discount["Amount"])
         )
-    item_obj.engraving_info = item["Title"]  # TODO чё надо?
+
+    item_obj.amount = _amount #TODO Отменённые заказы имеют quantity 0 при этом стоимость item и TotalOrder могут меть значения. проблема в подсчёте amount
+    item_obj.engraving_info = item["Title"]  # TODO На перспективу
 
 
 def _format_client(*,
                    order: dict,
                    client_obj: Client):
-    buyer_info = order.get("BuyerInfo")
-    if buyer_info:
-        client_obj.email = buyer_info["BuyerEmail"]
+    client_obj.email = order.get("BuyerInfo", {}).get("BuyerEmail")
 
 
 def _format_city(*,
                  order: dict,
                  city_obj: City):
-    shipping_address = order.get("ShippingAddress")
-    if shipping_address:
-        city_obj.name = shipping_address["City"]
-        city_obj.state = shipping_address["StateOrRegion"]
-        city_obj.country = shipping_address["CountryCode"]
+    shipping_address = order.get("ShippingAddress", {})
+    city_obj.name = shipping_address.get("City")
+    city_obj.state = shipping_address.get("StateOrRegion")
+    city_obj.country = shipping_address.get("CountryCode")
 
 
 def format_order_data(*,
@@ -68,12 +75,19 @@ def format_order_data(*,
         _format_good_in_order(item=item, item_obj=good_in_order)
 
         # calculate tax and quantity for order
-        order_obj.quantity += good_in_order.quantity
+        item_quantity = good_in_order.quantity
+        item_tax = item.get("ItemTax")
+        item_discount_tax = item.get("PromotionDiscountTax")
 
-        if item.get("ItemTax") and item.get("PromotionDiscountTax"):
-            order_obj.tax += (
-                (good_in_order.quantity * float(item["ItemTax"]["Amount"])) - float(item["PromotionDiscountTax"]["Amount"])
+        order_obj.quantity += item_quantity
+
+        # TODO Отменённые заказы имеют quantity 0 при этом стоимость item и TotalOrder могут меть значения. проблема в подсчёте amount
+        _amount_tax = 0.0
+        if item_tax and item_discount_tax and item_quantity:
+            _amount_tax += (
+                (item_quantity * float(item_tax["Amount"])) - float(item_discount_tax["Amount"])
             )
+        order_obj.tax = _amount_tax
 
         order_items.append(good_in_order)
 
