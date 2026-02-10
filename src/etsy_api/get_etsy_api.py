4db52a7df0f934ddacb902b95313682c1c4b3196
Syrnnik <sakh.alexandr@gmail.com>
2026-02-10 22:54:35 +0300

    fix: client id as just keystring

--- src/etsy_api/get_etsy_api.py
+++ src/etsy_api/get_etsy_api.py
@@ -7,13 +7,13 @@ from loguru import logger as log
 from typing_extensions import deprecated
 
 from configs.env import ETSY_API_KEY
-from constants.etsy_oauth import etsy_auth, STATE
+from constants.etsy_oauth import STATE, etsy_auth
 from constants.files_paths import SHOPS_DATA_FILE_PATH
 from schemes.access_token import AuthToken
 from schemes.auth import AuthCode
 from schemes.shop_data import ShopData
 
-AUTH_CODE_WAIT_TIME_IN_SECONDS = 15
+AUTH_CODE_WAIT_TIME_IN_SECONDS = 60
 AUTH_TOKEN_LIFE_TIME_IN_SECONDS = 3600
 
 
@@ -24,7 +24,7 @@ class SouvTechEtsyAPI(EtsyAPI):
         log.info(f"Custom refreshing Etsy access token..")
         data = {
             "grant_type": "refresh_token",
-            "client_id": self.keystring,
+            "client_id": self.keystring.split(":")[0],
             "refresh_token": self.refresh_token,
         }
         del self.session.headers["Authorization"]
@@ -72,30 +72,39 @@ def _save_auth_token(auth_token: AuthToken, shop_id: int):
             shop.shop_token = auth_token.access_token
             shop.shop_refresh_token = auth_token.refresh_token
             shop.expiry = auth_token.expires_at
-    with open(SHOPS_DATA_FILE_PATH, 'w') as f:
+    with open(SHOPS_DATA_FILE_PATH, "w") as f:
         json.dump([shop_data.model_dump() for shop_data in shops_data], f)
 
 
-def _get_auth_code(shop_id: int) -> AuthCode:
-    shop_data = _get_shop_data_by_id(shop_id)
+def _get_auth_code(
+    shop_id: int,
+) -> AuthCode:
+    shop_data = _get_shop_data_by_id(shop_id=shop_id)
 
     auth_code_exists = shop_data.shop_auth_code.strip()
     if not auth_code_exists:
         auth_url, _ = etsy_auth.get_auth_code()
         print(f"Auth code is not found. Open {auth_url} to grant access.")
         time.sleep(AUTH_CODE_WAIT_TIME_IN_SECONDS)
-        return _get_auth_code(shop_id)
+        return _get_auth_code(shop_id=shop_id)
 
     auth_code_response = AuthCode(code=shop_data.shop_auth_code)
     return auth_code_response
 
 
-def _get_auth_token(shop_id: int) -> AuthToken:
-    shop_data = _get_shop_data_by_id(shop_id)
+def _get_auth_token(
+    shop_id: int,
+    reset_auth_code: bool | None = False,
+) -> AuthToken:
+    shop_data = _get_shop_data_by_id(shop_id=shop_id)
 
-    auth_token_exists = shop_data.shop_token.strip() and shop_data.shop_refresh_token.strip() and shop_data.expiry != 0
-    if not auth_token_exists:
-        auth_code_response = _get_auth_code(shop_id)
+    auth_token_exists = (
+        shop_data.shop_token.strip()
+        and shop_data.shop_refresh_token.strip()
+        and shop_data.expiry != 0
+    )
+    if not auth_token_exists or reset_auth_code:
+        auth_code_response = _get_auth_code(shop_id=shop_id)
         etsy_auth.set_authorisation_code(
             code=auth_code_response.code,
             state=STATE,
@@ -103,12 +112,18 @@ def _get_auth_token(shop_id: int) -> AuthToken:
         auth_token_response = etsy_auth.get_access_token()
         log.info(f"Auth token response: {auth_token_response}")
         auth_token = AuthToken(
-            access_token=auth_token_response['access_token'],
-            refresh_token=auth_token_response['refresh_token'],
-            expires_at=auth_token_response['expires_at'],
+            access_token=auth_token_response["access_token"],
+            refresh_token=auth_token_response["refresh_token"],
+            expires_at=auth_token_response["expires_at"],
         )
-        _save_auth_token(auth_token, shop_id)
-        return _get_auth_token(shop_id)
+        _save_auth_token(
+            auth_token=auth_token,
+            shop_id=shop_id,
+        )
+        return _get_auth_token(
+            shop_id=shop_id,
+            reset_auth_code=reset_auth_code,
+        )
 
     auth_token = AuthToken(
         access_token=shop_data.shop_token,
@@ -119,7 +134,10 @@ def _get_auth_token(shop_id: int) -> AuthToken:
 
 
 @deprecated("Token refresh inside EtsyAPI, not needed")
-def refresh_auth_token(etsy_api: SouvTechEtsyAPI, shop_id: int):
+def refresh_auth_token(
+    etsy_api: SouvTechEtsyAPI,
+    shop_id: int,
+):
     new_access_token, new_refresh_token, new_expires_at = etsy_api.refresh()
     log.success(f"New access token: {new_access_token}")
     log.success(f"New refresh token: {new_refresh_token}")
@@ -132,8 +150,14 @@ def refresh_auth_token(etsy_api: SouvTechEtsyAPI, shop_id: int):
     _save_auth_token(new_auth_token, shop_id)
 
 
-def get_etsy_api(shop_id: int):
-    auth_token = _get_auth_token(shop_id)
+def get_etsy_api(
+    shop_id: int,
+    reset_auth_code: bool | None = False,
+) -> SouvTechEtsyAPI:
+    auth_token = _get_auth_token(
+        shop_id=shop_id,
+        reset_auth_code=reset_auth_code,
+    )
 
     etsy_api = SouvTechEtsyAPI(
         keystring=ETSY_API_KEY,
@@ -143,12 +167,21 @@ def get_etsy_api(shop_id: int):
     )
     etsy_api.shop_id = shop_id
     time.sleep(3)
+
     # try:
-    # etsy_api.ping()
+    #     etsy_api.ping()
     # except Unauthorised:
     #     log.warning(f"Token is expired. Requesting new token.")
-    #     time.sleep(10)
-    #     refresh_auth_token(etsy_api, shop_id)
-    #     return get_etsy_api(shop_id)
+    #     return get_etsy_api(
+    #         shop_id=shop_id,
+    #         # reset_auth_code=True,
+    #     )
 
     return etsy_api
+
+
+if __name__ == "__main__":
+    get_etsy_api(
+        shop_id=2,
+        reset_auth_code=True,
+    )
